d45ce48eb154931ee50d56f6ab7ba3001a13c0cb
Vicente <busslina@gmail.com>
2024-01-28 22:10:10 +0100

    snapshot

--- bin/general_test.dart
+++ bin/general_test.dart
@@ -3,7 +3,10 @@ import 'package:logger/logger.dart';
 void main(List<String> arguments) {
   print('Hello world');
 
-  final logger = Logger(level: Level.all);
+  final logger = Logger(
+    level: Level.all,
+    filter: ProductionFilter(),
+  );
 
   logger.i('Hello world');
 }
